0a605f4f23fbd45aa2e373920a9c53501445e86d
notrealtze <you@example.com>
2026-06-07 21:21:14 -0500

    Initial mod

---
+++ src/main/java/com/example/goldenapple/GoldenAppleMod.java
@@ -0,0 +1,12 @@
+package com.example.goldenapple;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(GoldenAppleMod.MOD_ID)
+public class GoldenAppleMod {
+    public static final String MOD_ID = "goldenapple";
+
+    public GoldenAppleMod(IEventBus modEventBus) {
+    }
+}
